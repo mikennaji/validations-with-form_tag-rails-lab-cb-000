8a2fb1152d22c82defce5d4dfb43ff80191b22e1
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-03 14:11:41 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,14 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+     @post.assign_attributes(post_params)
+    if @post.valid?
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+     @post.save
+     redirect_to post_path(@post)
+   else
+     render :edit
+   end
   end
 
   private
